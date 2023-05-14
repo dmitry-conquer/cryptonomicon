d3ba43b483a0a463a8fd578260ea8b92801d57d6
dmitry-conquer <dmitry.conquer@gmail.com>
2023-05-14 20:14:35 +0200

    fixed bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-<<<<<<< HEAD
-      require('@tailwindcss/forms'),
-=======
-     require('@tailwindcss/forms'),
->>>>>>> conquer-code
-    ],
-}
-
+  plugins: [require("@tailwindcss/forms")],
+};
